9948d179790515cee66044a6fb527bf5fb451625
Daniel <32684450+Daniel-Ronson@users.noreply.github.com>
2020-03-09 02:27:23 -0400

    web scraping and clean code

--- src/db/db_init.js
+++ src/db/db_init.js
@@ -7,22 +7,21 @@ require('./mongoose')
 
 //const data = (({name,area,capital,population,nativeName,flag,}) => ({name,area,capital,population,nativeName,flag,languages}))(body)
 
-
 request('https://en.wikipedia.org/wiki/ISO_3166-1#Current_codes', (error, response, html) => {
     if(!error && response.statusCode == 200) {
          var countries = []
         const $ = cheerio.load(html);
+
+        //iterate through each element in the table and extract the country codes
         $('#mw-content-text > div > table:nth-child(30) > tbody > tr > td:nth-child(3) > span').each((index,element) => {
-            var countryName = $(element).text()
 
-            getCountryData(countryName)
+            //convert element to text
+            var countryCode = $(element).text()
 
+            //Get data from public API and load into database
+            getCountryData(countryCode)
         })
 
     }
 }) 
 
-
-// getCountryData('USA', (error,data) =>{
-//     console.log(data.name)
-// })
